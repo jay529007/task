735b964d9eaa847333ffbd920343691f60009b0d
Jay <109064986+jay529007@users.noreply.github.com>
2022-11-02 18:26:15 +0530

    Update task_2.dart

--- lib/task/task_2.dart
+++ lib/task/task_2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class Task2 extends StatelessWidget {
   const Task2({super.key});
@@ -7,18 +8,27 @@ class Task2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Text(
-                "Hello, world!",
-                textDirection: TextDirection.ltr,
-              ),
+        child:Scaffold(
+          body: Container(
+            child: Column(
+              children: [
+                Center(
+                  child: Image.asset(
+                    'assets/4.jpg',
+                    width: 50.w,
+                  ),
+                ),
+          Image.asset('assets/5.jpg'),
+                Icon(
+                  Icons.safety_check,
+                  size: 9.h,
+                  color: Colors.cyan,
+                ),
+              ],
             ),
-          ],
+          ),
+          ),
         ),
-      ),
     );
   }
 }
