a0776cc4e069c4ceea56d0b0f922155198995dde
jay <jaysatapara.3@gmail.com>
2022-11-07 16:14:23 +0530

    Test App

--- lib/task/task_6.dart
+++ lib/task/task_6.dart
@@ -13,6 +13,8 @@ class _Task8State extends State<Task8> {
     return Scaffold(
       appBar: AppBar(),
       body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
             width: MediaQuery.of(context).size.width * 0.3,
@@ -21,18 +23,21 @@ class _Task8State extends State<Task8> {
             ),
           ),
           Container(
-
             width: MediaQuery.of(context).size.width * 0.3,
+            height: MediaQuery.of(context).size.height * 0.4,
             decoration: BoxDecoration(
               color: Colors.cyan,
             ),
           ),
           Container(
-            width: MediaQuery.of(context).size.width * 0.3,
+
+            width: MediaQuery.of(context).size.width * 0.2,
+            // height: MediaQuery.of(context).size.height * double.infinity,
             decoration: BoxDecoration(
               color: Colors.greenAccent,
             ),
           ),
+
         ],
       ),
     );
